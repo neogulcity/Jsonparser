235d5e8413511f5d1a52ad658c4ddf8e8062dc3c
neogulcity <neogulcity@gmail.com>
2022-09-06 10:33:10 +0900

    Add Data class

---
+++ src/Data.cpp
@@ -0,0 +1,17 @@
+#include "Data.h"
+
+std::string Data::GetPath() { return this->m_path; }
+
+std::string Data::GetName() { return this->m_name; }
+
+std::vector<std::string> Data::GetData() { return this->m_data; }
+
+std::vector<std::string>* Data::pGetData() { return &this->m_data; }
+
+void Data::SetPath(const std::string& _path) { this->m_path = _path; }
+
+void Data::SetName(const std::string& _name) { this->m_name = _name; }
+
+void Data::SetData(const std::vector<std::string>& _data) {
+    this->m_data = _data;
+}
---
+++ src/Data.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Data {
+   public:
+    Data(const std::string& _path, const std::string& _name,
+         const std::vector<std::string>& _data)
+        : m_path(_path), m_name(_name), m_data(_data) {}
+    ~Data() = default;
+
+    std::string GetPath();
+    std::string GetName();
+    std::vector<std::string> GetData();
+    std::vector<std::string>* pGetData();
+    void SetPath(const std::string& _path);
+    void SetName(const std::string& _name);
+    void SetData(const std::vector<std::string>& _data);
+
+   private:
+    std::string m_path;
+    std::string m_name;
+    std::vector<std::string> m_data;
+};
