a971bcc9594ddb4401ab4d868b6518f97d22c5cc
neogulcity <neogulcity@gmail.com>
2022-09-07 09:16:19 +0900

    Namespace added.

--- src/Data.h
+++ src/Data.h
@@ -1,25 +1,24 @@
 #pragma once
 
-#include <string>
-#include <vector>
+namespace jsonvalue {
+    class Data {
+    public:
+        Data(const std::string& _path, const std::string& _name,
+            const std::vector<std::string>& _data)
+            : m_path(_path), m_name(_name), m_data(_data) {}
+        ~Data() = default;
 
-class Data {
-   public:
-    Data(const std::string& _path, const std::string& _name,
-         const std::vector<std::string>& _data)
-        : m_path(_path), m_name(_name), m_data(_data) {}
-    ~Data() = default;
+        std::string GetPath();
+        std::string GetName();
+        std::vector<std::string> GetData();
+        std::vector<std::string>* pGetData();
+        void SetPath(const std::string& _path);
+        void SetName(const std::string& _name);
+        void SetData(const std::vector<std::string>& _data);
 
-    std::string GetPath();
-    std::string GetName();
-    std::vector<std::string> GetData();
-    std::vector<std::string>* pGetData();
-    void SetPath(const std::string& _path);
-    void SetName(const std::string& _name);
-    void SetData(const std::vector<std::string>& _data);
-
-   private:
-    std::string m_path;
-    std::string m_name;
-    std::vector<std::string> m_data;
-};
+    private:
+        std::string m_path;
+        std::string m_name;
+        std::vector<std::string> m_data;
+    };
+}
