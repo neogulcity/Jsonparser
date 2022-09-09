ea1f2e8f42c4a0e63835431036b247bc20603ff9
neogulcity <neogulcity@gmail.com>
2022-09-09 11:58:34 +0900

    Update Data.h

--- src/Data.h
+++ src/Data.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace jsonvalue {
     class Data {
-    public:
+       public:
         Data(const std::string& _path, const std::string& _name,
-            const std::vector<std::string>& _data)
+             const std::vector<std::string>& _data)
             : m_path(_path), m_name(_name), m_data(_data) {}
         ~Data() = default;
 
@@ -16,7 +19,7 @@ namespace jsonvalue {
         void SetName(const std::string& _name);
         void SetData(const std::vector<std::string>& _data);
 
-    private:
+       private:
         std::string m_path;
         std::string m_name;
         std::vector<std::string> m_data;
