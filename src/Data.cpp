e7b4688f5335d75970874155d96a5b83eec5bb14
neogulcity <neogulcity@gmail.com>
2022-09-09 11:58:28 +0900

    Update Data.cpp

--- src/Data.cpp
+++ src/Data.cpp
@@ -1,21 +1,12 @@
-#include <string>
-#include <vector>
-
 #include "Data.h"
 
 namespace jsonvalue {
     std::string Data::GetPath() { return this->m_path; }
-
     std::string Data::GetName() { return this->m_name; }
-
     std::vector<std::string> Data::GetData() { return this->m_data; }
-
     std::vector<std::string>* Data::pGetData() { return &this->m_data; }
-
     void Data::SetPath(const std::string& _path) { this->m_path = _path; }
-
     void Data::SetName(const std::string& _name) { this->m_name = _name; }
-
     void Data::SetData(const std::vector<std::string>& _data) {
         this->m_data = _data;
     }
